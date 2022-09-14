01ee0ead03982ad21d8cc2252c4d426129a055a5
Matheus <wallysom2@gmail.com>
2022-09-14 14:44:51 -0300

    feat: parte logica da pag de login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+  
+    return (
+      <div className="App">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Login />} />
+              </Routes>
+            </BrowserRouter>
+      </div>
+    );
+  }
+  export default App;

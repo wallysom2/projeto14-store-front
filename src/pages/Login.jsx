edd16741aec29ca4e147813167adf1a792bcdd14
Matheus <wallysom2@gmail.com>
2022-09-15 15:22:35 -0300

    feat: construindo botao para o fluxo de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import styled from "styled-components";
 
 import UserContext from '../contexts/UserContext';
 
@@ -18,33 +19,92 @@ function Login() {
       const { data } = await axios.post('http://localhost:5000/signIn', body);
 
       setUser(data);
-      navigator('/home');
+      navigator('/loja');
     } catch (error) {
       console.error('Erro ao fazer o login', error);
     }
   }
 
   return (
-    <div className="">
-      <form>
-        <input type="text" onChange={e => setEmail(e.target.value)} placeholder="Email" />
+    <StyledContainer >
+      <Title>
+        Login
+      </Title>
+      <Form>
+        <Input type="text" onChange={e => setEmail(e.target.value)} placeholder="Email" />
         <br />
-        <input
+        <Input
           type="password"
           onChange={e => setPassword(e.target.value)}
           placeholder="Senha"
         />
         <br />
-        <button type="submit" onClick={handleSubmit}>
+        <Button type="submit" onClick={handleSubmit}>
           Entrar
-        </button>
+        </Button>
         <p>
           <Link to="/cadastro" style={{ fontSize: 14, color: 'black' }}>
             Primeira vez? Cadastrate-se!
           </Link>
         </p>
-      </form>
-    </div>
+      </Form>
+    </StyledContainer>
   );
 }
 export default Login;
+
+const StyledContainer = styled.section`
+width: 100vw;
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+const Title = styled.p `
+font: 35px Bebas Neue;
+padding-bottom: 30px;
+`
+const Input = styled.input `
+width: 327px;
+height: 46px;
+margin: 7px;
+::placeholder {
+  font-size: 15px;
+  padding-left: 10px;
+}
+`
+const Form = styled.form `
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+const Button = styled.button `
+	box-shadow: 0px 1px 0px 0px #fff6af;
+	background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+	background-color:#ffec64;
+	border-radius:6px;
+	border:1px solid #ffaa22;
+	display:inline-block;
+	cursor:pointer;
+	color:#333333;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffee66;
+  width: 230px;
+  height: 40px;
+  margin-top:40px;
+  margin-bottom: 20px;
+  &:hover {
+	background:linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+	background-color:#ffab23;
+}
+&:active {
+	position:relative;
+	top:1px;
+}
+`

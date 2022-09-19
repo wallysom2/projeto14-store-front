7d39a51acacf2699d655fc96435ef24fa1a38306
Raphael Braga <rbragamouraa@gmail.com>
2022-09-19 12:57:06 -0300

    concerto da rora loja

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
                 <Routes>
                      <Route path="/" element={<Login />} /> 
                     <Route path="/cadastro" element={<Cadastro />} />
-                    <Route path="/" element={<Loja/>}></Route>
+                    <Route path="/loja" element={<Loja/>}></Route>
                     <Route path="/carrinho" element={<Carrinho/>}></Route>
                 </Routes>
                 </BrowserRouter>
